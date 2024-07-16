caa882e6196a6c1520c79ac1f54d687f2662f5e0
Paola Pappalardo <162357210+paolaapap@users.noreply.github.com>
2024-07-16 11:16:20 +0200

    Delete storage/framework/views directory

--- storage/framework/views/04bf65374c3ed0922faf8b0922e29eb7.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\paola\Desktop\Università\5. TERZO ANNO\Web programming\HomeWork\hw2\hw2\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
